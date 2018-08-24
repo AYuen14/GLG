8d818e681daa5aa9022c2f3ce2a79125b827fd6c
Adam Yuen <adamyuen07@yahoo.com>
2018-08-23 20:54:02 -0700

    Initial Commit

---
+++ GLGWorkSample/GLGWorkSample/App_Start/BundleConfig.cs
@@ -0,0 +1,28 @@
+﻿namespace GLGWorkSample
+{
+    using System.Web.Optimization;
+
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            #region ASP.Net MVC Script Bundles
+
+            bundles.Add(new ScriptBundle("~/bundles/layoutmain.js").Include(
+                "~/Scripts/jquery-{version}.js",
+                "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Scripts/jquery.validate*"));
+
+            #endregion
+
+            #region ASP.Net MVC Style Bundles
+
+            bundles.Add(new StyleBundle("~/bundles/layoutmain.css").Include(
+                "~/Content/main.css"));
+
+            #endregion
+        }
+    }
+}
